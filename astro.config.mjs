d8e1f56a5690035fb91d7f88258d99a3e9b359d1
Luke Nevins <ufpolis@gmail.com>
2026-06-02 12:18:06 -0400

    describe what changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://polisuf.org',
+  site: 'https://ufpolis.com',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
